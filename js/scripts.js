559d051e94b853b6603961fbee2096f8e9c0d887
AJ Ancheta and Alexander Kondra <aj@>
2019-03-26 15:43:30 -0700

    add definitions to js vocabulary and added highlight to each definition

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,110 @@
 $(document).ready(function() {
   $("#jsdefClick").click(function() {
     $("#jsdef").toggle();
+    $("#jsdef").toggleClass("highlight");
 
   });
 
 $("#opClick").click(function() {
   $("#opdef").toggle();
+  $("#opdef").toggleClass("highlight");
 
   });
+
+$("#vaClick").click(function() {
+  $("#vadef").toggle();
+  $("#vadef").toggleClass("highlight");
+
+  });
+
+$("#varClick").click(function() {
+  $("#varnamdef").toggle();
+  $("#varnamdef").toggleClass("highlight");
+
+  });
+
+$("#funClick").click(function() {
+  $("#fundef").toggle();
+  $("#fundef").toggleClass("highlight");
+
 });
+
+  $("#metClick").click(function() {
+    $("#metdef").toggle();
+    $("#metdef").toggleClass("highlight");
+
+});
+
+$("#arguClick").click(function() {
+  $("#argudef").toggle();
+  $("#argudef").toggleClass("highlight");
+
+  });
+
+  $("#parClick").click(function() {
+    $("#pardef").toggle();
+    $("#pardef").toggleClass("highlight");
+
+  });
+
+
+  $("#retClick").click(function() {
+    $("#retdef").toggle();
+    $("#retdef").toggleClass("highlight");
+
+  });
+
+  $("#chaClick").click(function() {
+    $("#chadef").toggle();
+    $("#chadef").toggleClass("highlight");
+
+  });
+
+    $("#strinClick").click(function() {
+      $("#strindef").toggle();
+      $("#strindef").toggleClass("highlight");
+
+  });
+
+    $("#booClick").click(function() {
+      $("#boodef").toggle();
+      $("#boodef").toggleClass("highlight");
+
+  });
+
+    $("#undClick").click(function() {
+      $("#unddef").toggle();
+      $("#unddef").toggleClass("highlight");
+  });
+
+    $("#naClick").click(function() {
+      $("#nadef").toggle();
+      $("#nadef").toggleClass("highlight");
+  });
+
+
+    $("#esClick").click(function() {
+      $("#esdef").toggle();
+      $("#esdef").toggleClass("highlight");
+  });
+
+    $("#alClick").click(function() {
+      $("#aldef").toggle();
+      $("#aldef").toggleClass("highlight");
+  });
+
+    $("#coClick").click(function() {
+      $("#codef").toggle();
+      $("#codef").toggleClass("highlight");
+  });
+
+    $("#jquClick").click(function() {
+      $("#jqudef").toggle();
+      $("#jqudef").toggleClass("highlight");
+  });
+
+    $("#attClick").click(function() {
+      $("#attdef").toggle();
+      $("#attdef").toggleClass("highlight");
+  });
+    });
